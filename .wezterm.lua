181816413b128487498a5edff0daed149d85d410
Gabriel Fonseca <gabriel.f.alexandre@hotmail.com>
2024-10-21 10:29:37 -0300

    wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,11 +3,12 @@ local config = {}
 
 
 config.font = wezterm.font 'FiraCode Nerd Font'
-config.window_background_opacity = 0.85
+config.window_background_opacity = 0.80
 
-window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.default_cursor_style = 'BlinkingBlock'
+config.animation_fps = 180
+
 config.enable_tab_bar = false
-
 config.color_scheme = 'Tokyo Night'
 
 return config
